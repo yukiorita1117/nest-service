8ee6e714813f35f5e38e152e27b5a0e09421a943
折田幸夫 <3204704@gmail.com>
2022-04-20 22:41:58 +0900

    add users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 // デコレータ: ＠がついているもの。デコレーションする。
 // providersに設定したものは contorollersで使用できるようになる
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
